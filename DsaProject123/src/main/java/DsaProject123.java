305cc3c342ea0b4d4d88191f26c337dcd8406e6b
krams-code <manabatmarkaaron111@gmail.com>
2024-10-21 17:31:56 +0800

    Add files via upload

---
+++ DsaProject123/src/main/java/DsaProject123.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author kristine heart
+ */
+public class DsaProject123 {
+
+    public static void main(String[] args) {
+            adminSearchAndDelete sub = new adminSearchAndDelete();
+       sub.setVisible(true);
+        
+//        userInformation third = new userInformation();
+//        userInformation.setVisible(true);
+
+    }
+}
