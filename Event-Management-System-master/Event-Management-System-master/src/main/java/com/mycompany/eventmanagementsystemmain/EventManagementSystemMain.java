1dec67e96f2a51d6d9ef6cd7ba8bb491aa1f3872
Arriane Nichole Caraliman <ayancaraliman@gmail.com>
2025-01-14 12:47:43 +0800

    Add files via upload

---
+++ Event-Management-System-master/Event-Management-System-master/src/main/java/com/mycompany/eventmanagementsystemmain/EventManagementSystemMain.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-de^fault.txt to change this license
+ */
+
+package com.mycompany.eventmanagementsystemmain;
+
+
+public class EventManagementSystemMain {
+
+ public static void main(String[] args) {    
+  new welcomePage();
+   
+    }
+} 
